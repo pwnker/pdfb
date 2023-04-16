9d8bb69a2bc5e6abcbd66399f56b4072bd69d07e
TilenOkretic <tilen.okretic@gmail.com>
2023-04-16 16:33:54 +0200

    :tada: inital

--- dist/Assert.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function assert(condition, message) {
-    if (condition === false) {
-        throw new Error(message);
-    }
-}
-exports.default = assert;
-//# sourceMappingURL=Assert.js.map
